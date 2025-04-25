c417f7eeddd3c8dc3c5b25f9274a516f73b8b505
Sahil Khan <sahilkhan.83399@gmail.com>
2025-04-25 11:22:06 +0530

    setting cors allowed origin

--- organ_donation/settings.py
+++ organ_donation/settings.py
@@ -63,6 +63,9 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://revibe-organ.netlify.app",  # Replace with your actual Netlify URL
+]
 
 
 ROOT_URLCONF = 'organ_donation.urls'
